275238b3fc46e4bc7b2ec13c1cb14bd99d64c1e7
annyn <ennibondarenko@gmail.com>
2023-02-25 14:33:55 +0400

    commit

---
+++ calc.js
@@ -0,0 +1,15 @@
+const bill = document.getElementById("bill");
+const percentage = document.getElementsByClassName("percentage");
+const custom = document.getElementById("custom");
+const numberOfPeople = document.getElementById("numberOfPeople");
+
+    
+function tipClick(){
+    for(let i = 0; i < percentage; i++){
+        percentage.addEventListener("click",  (event) => {
+            
+        });
+     }
+
+}
+
